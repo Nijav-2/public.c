3fe4d71262232a010a423e20a328b9bc65fff8fc
Nijav-2 <nijavardhancseace@gmail.com>
2025-09-27 21:57:26 +0530

    Add files via upload

---
+++ car details.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+struct car{
+    int fuel_tank_cap;
+    int seating_cap;
+    float city_mileage;
+};
+int main(){
+    struct car c[2];
+    int i;
+    for(i=0;i<2;i++)
+    {
+        printf("Enter the car %d fuel tank capacity:\n",i+1);
+        scanf("%d",&c[i].fuel_tank_cap);
+        printf("Enter the car %d seating capacity:\n",i+1);
+        scanf("%d",&c[i].seating_cap);
+        printf("Enter the car %d city mileage:\n",i+1);
+        scanf("%f",&c[i].city_mileage);
+    }
+    printf("\n");
+    for(i=0;i<2;i++)
+    {
+        printf("car %d details:\n",i+1);
+        printf("fuel tank capacity:%d\n",c[i].fuel_tank_cap);
+        printf("seating capacity:%d\n",c[i].seating_cap);
+        printf("city mileage:%f\n",c[i].city_mileage);
+    }
+    return 0;
+}
